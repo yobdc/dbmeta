8dbe56d995f56b7b4843207b5d94c693c37702ba
yobdc <yobdc.101@gmail.com>
2018-06-20 21:06:08 +0800

    db_database添加jdbc_url字段

--- src/main/resources/create_tables.sql
+++ src/main/resources/create_tables.sql
@@ -1,105 +1,135 @@
 /*
-SQLyog Community v12.4.3 (64 bit)
-MySQL - 5.7.13-log : Database - yobdc
-*********************************************************************
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : localhost:3306
+ Source Schema         : dbmeta
+
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : 65001
+
+ Date: 20/06/2018 21:05:17
 */
 
-/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`yobdc` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `yobdc`;
-
-/*Table structure for table `db_column` */
-
+-- ----------------------------
+-- Table structure for db_column
+-- ----------------------------
 DROP TABLE IF EXISTS `db_column`;
-
-CREATE TABLE `db_column` (
+CREATE TABLE `db_column`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  `comment` varchar(255) DEFAULT NULL,
-  `table_id` bigint(20) DEFAULT NULL,
-  `database_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `table_id` bigint(20) NULL DEFAULT NULL,
+  `database_id` bigint(20) NULL DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `nullable` tinyint(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-/*Table structure for table `db_database` */
+-- ----------------------------
+-- Records of db_column
+-- ----------------------------
+INSERT INTO `db_column` VALUES (1, 'id', 'ID', '指针', 1, 1, 'bigint', 0);
+INSERT INTO `db_column` VALUES (2, 'name', '名称', '', 1, 1, 'varchar(255)', 1);
+INSERT INTO `db_column` VALUES (3, 'secuname', '债券名称', '撒大声地', 1, 1, 'varchar(255)', 1);
 
+-- ----------------------------
+-- Table structure for db_database
+-- ----------------------------
 DROP TABLE IF EXISTS `db_database`;
-
-CREATE TABLE `db_database` (
+CREATE TABLE `db_database`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  `comment` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `jdbc_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-/*Table structure for table `db_table` */
+-- ----------------------------
+-- Records of db_database
+-- ----------------------------
+INSERT INTO `db_database` VALUES (1, 'orderdb', '交易系统', NULL, NULL);
+INSERT INTO `db_database` VALUES (2, 'accountdb', '账户系统', NULL, NULL);
+INSERT INTO `db_database` VALUES (3, 'performancedb', '行情系统', NULL, NULL);
 
+-- ----------------------------
+-- Table structure for db_table
+-- ----------------------------
 DROP TABLE IF EXISTS `db_table`;
-
-CREATE TABLE `db_table` (
+CREATE TABLE `db_table`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  `comment` varchar(255) DEFAULT NULL,
-  `database_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `database_id` bigint(20) NULL DEFAULT NULL,
+  `update_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-/*Table structure for table `sys_permission` */
+-- ----------------------------
+-- Records of db_table
+-- ----------------------------
+INSERT INTO `db_table` VALUES (1, 'order', '订单', NULL, 1, '2018-03-11 15:11:02');
+INSERT INTO `db_table` VALUES (2, 'order_log', '订单物流', NULL, 1, '2018-06-17 19:00:51');
+INSERT INTO `db_table` VALUES (3, 'customer', '客户', NULL, 2, '2018-06-07 19:00:56');
+INSERT INTO `db_table` VALUES (4, 'custbaseinfo', '客户基本信息', NULL, 2, '2018-06-01 19:01:01');
 
+-- ----------------------------
+-- Table structure for sys_permission
+-- ----------------------------
 DROP TABLE IF EXISTS `sys_permission`;
-
-CREATE TABLE `sys_permission` (
+CREATE TABLE `sys_permission`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `code` varchar(50) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `code` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-/*Table structure for table `sys_role` */
-
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
-
-CREATE TABLE `sys_role` (
+CREATE TABLE `sys_role`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-/*Table structure for table `sys_role_permission` */
-
+-- ----------------------------
+-- Table structure for sys_role_permission
+-- ----------------------------
 DROP TABLE IF EXISTS `sys_role_permission`;
-
-CREATE TABLE `sys_role_permission` (
+CREATE TABLE `sys_role_permission`  (
   `permission_id` bigint(20) NOT NULL,
   `role_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`permission_id`,`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`permission_id`, `role_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-/*Table structure for table `sys_user` */
-
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
-
-CREATE TABLE `sys_user` (
+CREATE TABLE `sys_user`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) DEFAULT NULL,
-  `nickname` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `username` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `nickname` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES (1, 'admin', '管理员', 'admin');
+
+SET FOREIGN_KEY_CHECKS = 1;
