81974c4891944f18585c576972baa1fb2540e087
yobdc <yobdc.101@gmail.com>
2018-06-20 21:28:31 +0800

    fix typo

--- src/main/java/com/yobdc/util/db/MetaUtil.java
+++ src/main/java/com/yobdc/util/db/MetaUtil.java
@@ -1,15 +1,16 @@
 package com.yobdc.util.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MetaUtil {
-    public static void load() {
+    public static void showColumns() {
+        Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/dbmeta?user=root&password=root");
-            conn.getMetaData();
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/dbmeta?user=root&password=root");
+            DatabaseMetaData meta = conn.getMetaData();
+            ResultSet rs = meta.getColumns(null, "%", "db_table", "%");
+            rs.toString();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
@@ -18,6 +19,14 @@ public class MetaUtil {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
