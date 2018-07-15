18295ed405e50c0acf091fa13aaa5e691e1dac51
yobdc <yobdc.101@gmail.com>
2018-07-15 08:24:47 +0800

    修复database编辑保存时的bug

--- src/main/java/com/yobdc/kit/db/MetaKit.java
+++ src/main/java/com/yobdc/kit/db/MetaKit.java
@@ -1,7 +1,19 @@
 package com.yobdc.kit.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MetaKit {
     public static boolean testConnection(String jdbcUrl) {
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl);
+            String dbProductName = connection.getMetaData().getDatabaseProductName();
+            String dbProductVersion = connection.getMetaData().getDatabaseProductVersion();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
