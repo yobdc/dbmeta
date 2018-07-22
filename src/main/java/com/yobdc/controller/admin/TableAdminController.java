74084eda6f049d636755e7834d6d38e5d8eace0f
Lex <yobdc.101@gmail.com>
2018-07-22 15:00:47 +0800

    fix typo

--- src/main/java/com/yobdc/controller/admin/TableAdminController.java
+++ src/main/java/com/yobdc/controller/admin/TableAdminController.java
@@ -6,5 +6,6 @@ public class TableAdminController extends BaseController {
     public final static String CONTROLLER_KEY = "/admin/table";
 
     public void index() {
+        renderFreeMarker("/views/pages/admin/table/list.ftl");
     }
 }
