27abe7cda8467013977a66a0c0f22f36c41b413c
Lex <yobdc.101@gmail.com>
2018-07-26 08:42:37 +0800

    fix typo

--- src/main/java/com/yobdc/controller/admin/TableAdminController.java
+++ src/main/java/com/yobdc/controller/admin/TableAdminController.java
@@ -1,6 +1,7 @@
 package com.yobdc.controller.admin;
 
 import com.yobdc.controller.BaseController;
+import com.yobdc.model.Table;
 
 public class TableAdminController extends BaseController {
     public final static String CONTROLLER_KEY = "/admin/table";
@@ -8,5 +9,12 @@ public class TableAdminController extends BaseController {
     public void index() {
         Long databaseId = getParaToLong(0);
         renderFreeMarker("/views/pages/admin/table/list.ftl");
+    }
+
+    public void edit() {
+        Long tableId = getParaToLong(0);
+        Table table = Table.dao.findById(tableId);
+        setAttr("table", table);
+        renderFreeMarker("/views/pages/admin/table/edit.ftl");
     }
 }
